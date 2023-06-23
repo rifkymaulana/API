4f67433961aef863640946273f5e49be12ed7bef
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-06-23 14:59:49 +0700

    feat: implement generic for repositories and contracts

--- API/Repositories/RoomRepository.cs
+++ API/Repositories/RoomRepository.cs
@@ -4,7 +4,7 @@ using API.Models;
 
 namespace API.Repositories;
 
-public class RoomRepository : BaseRepository<Room>
+public class RoomRepository : BaseRepository<Room>, IRoomRepository
 {
     public RoomRepository(ApplicationDbContext context) : base(context)
     {
